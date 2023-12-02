e2b64c4fe1f7effb158b31bc5adfac1458ede9a8
gitlalihi <t6916417@gmail.com>
2023-12-02 14:55:14 +0530

    Intial commit

---
+++ matrix_multiplication.py
@@ -0,0 +1,26 @@
+#Given two square matrices A and B of size n x n each, find their multiplication matrix. 
+def matrix_multiply(A, B):
+    n = len(A)
+    result = [[0] * n for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                result[i][j] += A[i][k] * B[k][j]
+
+    return result
+
+
+A = [[1, 2, 3],
+     [4, 5, 6],
+     [7, 8, 9]]
+
+B = [[9, 8, 7],
+     [6, 5, 4],
+     [3, 2, 1]]
+
+
+result = matrix_multiply(A, B)
+
+
+for row in result:
+    print(row)
